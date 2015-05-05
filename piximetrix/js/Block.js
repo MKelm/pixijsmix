45b55f800c97a202a23528783008058650f346fa
Martin Kelm <martinkelm@shrt.ws>
2015-05-05 12:09:03 +0200

    Extensions in block logic

--- piximetrix/js/Block.js
+++ piximetrix/js/Block.js
@@ -1,10 +1,59 @@
-Metrix.Block = function() {
-  this.x = 0;
-  this.y = 0;
+Metrix.Block = function(parent, size) {
+  this.parent = parent;
+  this.size = size;
+  this.position = {
+    column: -1,
+    row: -1
+  };
+  
+  this.gfx = new PIXI.Graphics();
+  this.gfx.visible = false;
+  
+  this.colorIdx = -1;
+  this.colors = [
+    0xFF6900, // orange
+    0xCA0312, // red
+    0x179200, // green
+    0xFFED3F, // yellow
+    0x3F41FF, // blue
+    0xFF3F87 // violet
+  ];
 };
 
 Metrix.Block.prototype.constructor = Metrix.Block;
 
-Metrix.Block.prototype.setPos = function(x, y) {
+Metrix.Block.prototype.setRandomColor = function() {
+  this.colorIdx = Math.round(Math.random() * (this.colors.length - 1));  
+};
 
-}
+Metrix.Block.prototype.setPosition = function(column, row) {
+  this.position.column = column;
+  this.position.row = row;
+  this.gfx.position.x = window.innerWidth / 2 - this.size * this.parent.columns / 2 + column * this.size;
+  this.gfx.position.y = row * this.size;
+};
+
+Metrix.Block.prototype.setGfx = function() {
+  this.gfx.beginFill(this.colors[this.colorIdx], 1);
+  this.gfx.drawRect(
+    this.parent.pixi.screenScale * 0.5, this.parent.pixi.screenScale * 0.5,
+    this.size - this.parent.pixi.screenScale, this.size - this.parent.pixi.screenScale
+  );
+  this.parent.pixi.stage.addChild(this.gfx);
+};
+
+Metrix.Block.prototype.isVisible = function() {
+  return this.gfx.visible;
+};
+
+Metrix.Block.prototype.show = function() {
+  this.gfx.visible = true;
+};
+
+Metrix.Block.prototype.hide = function() {
+  this.gfx.visible = false;
+};
+
+Metrix.Block.prototype.moveDown = function() {
+  this.setPosition(this.position.column, this.position.row+1);
+};
