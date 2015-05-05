cb25e6cef08b35d72d792dfa80e233b05b2814f4
Martin Kelm <martinkelm@shrt.ws>
2015-05-05 12:10:07 +0200

    More board logic and use grid and block objects

--- piximetrix/js/Board.js
+++ piximetrix/js/Board.js
@@ -1,50 +1,31 @@
 Metrix.Board = function(pixi) {
   this.pixi = pixi;
+  this.updateTime = 1000;
 
-  // grid columns / rows
-  this.gridColumns = 8;
-  this.gridRows = 12;
-  this.gridElementSize = 0; // calc on addGrid
+  // columns / rows
+  this.columns = 8;
+  this.rows = 12;
 
-  this.elements = [];
-  this.elementColors = [
-    0xFF6900, // orange
-    0xCA0312, // red
-    0x179200, // green
-    0xFFED3F, // yellow
-    0x3F41FF, // blue
-    0xFF3F87 // violet
-  ];
+  // blocks[columns][rows]
+  this.blockSize = this.pixi.screenScale * 100 / this.rows;
+  this.blocks = new Array(this.columns * this.rows);
+  for (var i = 0; i < this.columns; i++) {
+    this.blocks[i] = new Metrix.Block(this, this.blockSize);
+  }
+  this.blocksMaxIndex = -1;
+  
+  // grid to manage blocks in fields
+  this.grid = new Metrix.Grid(this, this.columns, this.rows);
 };
 
 Metrix.Board.prototype.constructor = Metrix.Board;
 
-Metrix.Board.prototype.addGridElement = function() {
-  var elementGfx = new PIXI.Graphics();
-
-  var randPos = Math.round(Math.random() * (this.gridColumns - 1));
-
-  elementGfx.position.x = window.innerWidth / 2 -
-    this.gridElementSize * this.gridColumns / 2 + randPos * this.gridElementSize;
-  elementGfx.position.y = 0;
-
-  elementGfx.beginFill(this.elementColors[0], 1);
-  elementGfx.drawRect(
-    this.pixi.screenScale * 0.5, this.pixi.screenScale * 0.5,
-    this.gridElementSize - this.pixi.screenScale,
-    this.gridElementSize - this.pixi.screenScale
-  );
-
-  this.pixi.stage.addChild(elementGfx);
-};
-
-Metrix.Board.prototype.addGrid = function() {
+Metrix.Board.prototype.initialize = function() {
 
   // set board grid and col / row size
   var gridGfx = new PIXI.Graphics();
   var gridGfxHeight = this.pixi.screenScale * 100;
-  this.gridElementSize = gridGfxHeight / this.gridRows;
-  var gridGfxWidth = this.gridElementSize * this.gridColumns;
+  var gridGfxWidth = gridGfxHeight / this.rows * this.columns;
 
   var gridStartX = window.innerWidth / 2 - gridGfxWidth / 2;
   var gridStartY = window.innerHeight / 2 - gridGfxHeight / 2;
@@ -58,20 +39,56 @@ Metrix.Board.prototype.addGrid = function() {
 
   // set grid lines
   gridGfx.lineStyle(this.pixi.screenScale * 0.5, 0x353535, 1);
-  for (var gCol = 0; gCol < this.gridColumns; gCol++) {
-    gridGfx.moveTo(gridStartX + gCol * this.gridElementSize, gridStartY);
+  for (var gCol = 0; gCol < this.columns; gCol++) {
+    gridGfx.moveTo(gridStartX + gCol * this.blockSize, gridStartY);
     gridGfx.lineTo(
-      gridStartX + gCol * this.gridElementSize,
-      gridStartY + this.gridElementSize * this.gridRows
+      gridStartX + gCol * this.blockSize,
+      gridStartY + this.blockSize * this.rows
     );
   }
-  for (var gRow = 0; gRow < this.gridRows + 1; gRow++) {
-    gridGfx.moveTo(gridStartX, gridStartY + gRow * this.gridElementSize);
+  for (var gRow = 0; gRow < this.rows + 1; gRow++) {
+    gridGfx.moveTo(gridStartX, gridStartY + gRow * this.blockSize);
     gridGfx.lineTo(
-      gridStartX + this.gridColumns * this.gridElementSize,
-      gridStartY + gRow * this.gridElementSize
+      gridStartX + this.columns * this.blockSize,
+      gridStartY + gRow * this.blockSize
     );
   }
 
   this.pixi.stage.addChild(gridGfx);
+};
+
+Metrix.Board.prototype.addRandomBlock = function() {
+  var blockRandColumn = Math.round(Math.random() * (this.columns - 1));
+  this.addBlock(blockRandColumn, 0);
+};
+
+Metrix.Board.prototype.addBlock = function(column, row) {
+  this.blocksMaxIndex++;
+  this.blocks[this.blocksMaxIndex].setPosition(column, row);
+  this.blocks[this.blocksMaxIndex].setRandomColor();
+  this.blocks[this.blocksMaxIndex].setGfx();
+  this.blocks[this.blocksMaxIndex].show();
+  // register block as grid field
+  this.grid.setBlockIndex(column, row, this.blocksMaxIndex);
+  this.grid.setMovementField(column, row); // every new block is located in a movement field
+};
+
+Metrix.Board.prototype.moveBlocks = function(enableMovement) {
+  if (enableMovement === true) {
+    for (var i = 0; i < this.grid.columns; i++) {
+      // scan from bottom to top to avoid extra movements
+      for (var j = this.grid.rows - 1; j > -1; j--) {
+        var blockIndex = this.grid.setFieldDown(i, j);
+        if (blockIndex > -1) {
+          this.blocks[blockIndex].moveDown();
+        } else if (this.grid.isMovementField(i, j) == true) {
+          enableMovement = false;
+        }
+      }
+    }
+  } 
+  if (enableMovement !== true)
+    this.addRandomBlock();
+  var scope = this;
+  setTimeout(function() { scope.moveBlocks(true); }, scope.updateTime);
 };
