6d080fce434b78eb28e7e40a46ff43e889fcdfd9
Martin Kelm <martinkelm@shrt.ws>
2015-05-06 13:58:16 +0200

    new class to create text elements

---
+++ piximetrix/js/Text.js
@@ -0,0 +1,34 @@
+Metrix.Text = function(parent, text, column, row, align) {
+  this.parent = parent;
+  
+  this.text = new PIXI.Text(
+    text,
+    { font: 'bold 20px Arial', fill: '#FFFFFF', align: 'center',
+      stroke: '#353535', strokeThickness: 6 }
+  );
+
+  if (align == "left") {
+    this.text.anchor.set(0, 0.5);
+  } else if (align == "right") {
+    this.text.anchor.set(1, 0.5);
+  } else {
+    this.text.anchor.set(0.5, 0.5);
+  }
+  this.text.position.x = window.innerWidth / 2 - this.parent.blockSize * this.parent.columns / 2 + this.parent.blockSize * (column - 1);
+  this.text.position.y = (row - 1) * this.parent.blockSize + this.parent.blockSize / 2;
+  
+  this.parent.pixi.stage.addChild(this.text);
+};
+
+Metrix.Text.prototype.constructor = Metrix.Text;
+
+Metrix.Text.prototype.setInteraction = function() {
+  this.text.interactive = true;
+  this.text.click = function(){
+    alert("click text");
+  };
+};
+
+Metrix.Text.prototype.setText = function(text) {
+  this.text.setText(text);
+}
