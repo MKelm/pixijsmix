5b0bb6746d290bb613ccfe561b86f967f9192711
Martin Kelm <martinkelm@shrt.ws>
2015-05-06 13:59:49 +0200

    changes to add gfx to container

--- piximetrix/js/Grid.js
+++ piximetrix/js/Grid.js
@@ -20,6 +20,43 @@ Metrix.Grid = function(parent, columns, rows) {
 
 Metrix.Grid.prototype.constructor = Metrix.Grid;
 
+Metrix.Grid.prototype.addGfx = function() {
+
+  // set board grid and col / row size
+  var gridGfx = new PIXI.Graphics();
+  var gridGfxHeight = this.parent.pixi.screenScale * 100;
+  var gridGfxWidth = gridGfxHeight / this.rows * this.columns;
+
+  var gridStartX = window.innerWidth / 2 - gridGfxWidth / 2;
+  var gridStartY = window.innerHeight / 2 - gridGfxHeight / 2;
+
+  // set grid background
+  gridGfx.beginFill(0xFFFFFF, 1);
+  gridGfx.drawRect(
+    gridStartX, gridStartY,
+    gridGfxWidth, gridGfxHeight
+  );
+
+  // set grid lines
+  gridGfx.lineStyle(this.parent.pixi.screenScale * 0.5, 0x353535, 1);
+  for (var gCol = 0; gCol < this.columns + 1; gCol++) {
+    gridGfx.moveTo(gridStartX + gCol * this.parent.blockSize, gridStartY);
+    gridGfx.lineTo(
+      gridStartX + gCol * this.parent.blockSize,
+      gridStartY + this.parent.blockSize * this.rows
+    );
+  }
+  for (var gRow = 0; gRow < this.rows + 1; gRow++) {
+    gridGfx.moveTo(gridStartX, gridStartY + gRow * this.parent.blockSize);
+    gridGfx.lineTo(
+      gridStartX + this.columns * this.parent.blockSize,
+      gridStartY + gRow * this.parent.blockSize
+    );
+  }
+
+  this.parent.container.addChild(gridGfx);
+};
+
 Metrix.Grid.prototype.setBlockIndex = function(column, row, index) {
   this.fields[column][row] = index;
 };
